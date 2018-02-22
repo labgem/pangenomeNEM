cfaa18fb279f8459289dac820fedf1dbc1ff8249
GAUTREAU Guillaume <guillaume.gautreau@free.fr>
2018-02-22 17:02:59 +0100

    add nem.pyx

---
+++ ppanggolin/NEM/nem.pyx
@@ -0,0 +1,13 @@
+cdef extern from "nem_exe.h":
+   cpdef int nem(const char* Fname,
+        const int nk,
+        const char* algo,
+        const float beta,
+        const char* convergence,
+        const float convergence_th,
+        const char* format,
+        const int it_max,
+        const int dolog,
+        const char* model_family,
+        const char* proportion,
+        const char* dispersion);
